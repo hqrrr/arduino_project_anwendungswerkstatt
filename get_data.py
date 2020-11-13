82a248e49f823263c45374e19847a4aa10979c05
hqr <71382064+hqrrr@users.noreply.github.com>
2020-11-13 20:34:48 +0100

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,15 +1,17 @@
+
 # -*- coding: utf-8 -*-
 # =================================
 # Python 3
-# Spyder 3.3.6
+# Jupyter Notebook 6.0.0
 # Author: hqrrr
 # Date of last update: Nov. 2020
 # =================================
 import serial
 import matplotlib.pyplot as plt
-from drawnow import * # real time plot lib, pip install drawnow
+from drawnow import *
 import atexit
 import time
+from IPython import display
 
 # =================================
 #            settings
@@ -32,32 +34,44 @@ plt.ion()
 # font size of the point label
 fz = 7
 
+
+
+# =================================
 values = []
-temperature_dallas = []
+temperature_dallas_0 = []
+temperature_dallas_1 = []
+temperature_dallas_2 = []
 temperature = []
 pressure = []
 altimeter = []
 humidity = []
 
 # functions
-def doAtExit():
-    arduino.close() # close arduino serial port
-    print("Close serial")
-    print("arduino.isOpen() = " + str(arduino.isOpen())) # True: open, False: closed
+#def doAtExit():
+#    arduino.close() # close arduino serial port
+#    print("Close serial")
+#    print("arduino.isOpen() = " + str(arduino.isOpen())) # True: open, False: closed
     
 def plotValues():
     plt.subplots_adjust(wspace = 0, hspace = 0.75)
-    
+    plt.rcParams['figure.figsize'] = [10, 10]
+    plt.rcParams['figure.dpi'] = 150
     plt.subplot(411)
     plt.title("temperature") # plot title
     plt.grid(True) # show grid, True or False
     plt.ylabel("temperature [$^\circ$C]") # label of axis y
-    plt.plot(temperature_dallas,"-",label="dallas")
+    plt.plot(temperature_dallas_0,"-",label="dallas_0")
+    plt.plot(temperature_dallas_1,"-",label="dallas_1")
+    plt.plot(temperature_dallas_2,"-",label="dallas_2")
     plt.plot(temperature,"-",label="bme280")
     plt.legend(loc="best") # label location
     
     # label data points on plot
-    for x, y in enumerate(temperature_dallas):
+    for x, y in enumerate(temperature_dallas_0):
+        plt.text(x, y+0.001, '%.1f' % y, ha='center', va= 'bottom',fontsize=fz)
+    for x, y in enumerate(temperature_dallas_1):
+        plt.text(x, y+0.001, '%.1f' % y, ha='center', va= 'bottom',fontsize=fz)
+    for x, y in enumerate(temperature_dallas_2):
         plt.text(x, y+0.001, '%.1f' % y, ha='center', va= 'bottom',fontsize=fz)
     for x, y in enumerate(temperature):
         plt.text(x, y+0.001, '%.1f' % y, ha='center', va= 'bottom',fontsize=fz)
@@ -97,17 +111,19 @@ def plotValues():
     
     
     
-atexit.register(doAtExit) # if program exit, do function doAtExit to close arduino serial port
+#atexit.register(doAtExit) # if program exit, do function doAtExit to close arduino serial port
 
 #print("arduino.isOpen() = " + str(arduino.isOpen())) # True: open, False: closed
 
 # pre-load some dummy data
 for i in range(0,50): 
-    temperature_dallas.append(0)
-    temperature.append(0)
-    pressure.append(0)
-    altimeter.append(0)
-    humidity.append(0)
+    temperature_dallas_0.append(20)
+    temperature_dallas_1.append(20)
+    temperature_dallas_2.append(20)
+    temperature.append(20)
+    pressure.append(900)
+    altimeter.append(150)
+    humidity.append(30)
 
 while True:
     time.sleep(1)  # 1-sec delay for each loop
@@ -126,18 +142,24 @@ while True:
             
             if value >= 0:
                 if i == 0:
-                    temperature_dallas.append(value) # add the value into list
-                    temperature_dallas.pop(0) # delete the first value in the list
+                    temperature_dallas_0.append(value) # add the value into list
+                    temperature_dallas_0.pop(0) # delete the first value in the list
                 elif i == 1:
+                    temperature_dallas_1.append(value)
+                    temperature_dallas_1.pop(0)
+                elif i == 2:
+                    temperature_dallas_2.append(value)
+                    temperature_dallas_2.pop(0)
+                elif i == 3:
                     temperature.append(value)
                     temperature.pop(0)
-                elif i == 2:
+                elif i == 4:
                     pressure.append(value)
                     pressure.pop(0)
-                elif i == 3:
+                elif i == 5:
                     altimeter.append(value)
                     altimeter.pop(0)
-                elif i == 4:
+                elif i == 6:
                     humidity.append(value)
                     humidity.pop(0)
                 else:
@@ -147,6 +169,7 @@ while True:
                 print("Invalid! negative number")
         
         drawnow(plotValues)
+        display.clear_output(wait=True)
         
     except ValueError:
         print("Invalid! cannot cast")
