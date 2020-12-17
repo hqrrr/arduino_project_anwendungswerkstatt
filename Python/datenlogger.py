70e596a66807fc78779abd59213bdbcbe7612fa0
hqr <71382064+hqrrr@users.noreply.github.com>
2020-12-17 23:35:22 +0100

    update GUI

--- Python/datenlogger.py
+++ Python/datenlogger.py
@@ -17,11 +17,8 @@ class MyMainForm(QMainWindow, Ui_Form):
         self.setupUi(self)
 
 if __name__ == "__main__":
-    #固定的，PyQt5程序都需要QApplication对象。sys.argv是命令行参数列表，确保程序可以双击运行
     app = QApplication(sys.argv)
-    #初始化
+    # initialization
     myWin = MyMainForm()
-    #将窗口控件显示在屏幕上
     myWin.show()
-    #程序运行，sys.exit方法确保程序完整退出。
     sys.exit(app.exec_())
