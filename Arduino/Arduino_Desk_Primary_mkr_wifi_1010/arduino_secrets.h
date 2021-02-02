2991f91753d071e377a1eed68656ac8fffdb78af
hqr <71382064+hqrrr@users.noreply.github.com>
2021-02-02 10:41:52 +0100

    Update arduino_secrets.h

--- Arduino/Arduino_Desk_Primary_mkr_wifi_1010/arduino_secrets.h
+++ Arduino/Arduino_Desk_Primary_mkr_wifi_1010/arduino_secrets.h
@@ -1,2 +1,2 @@
-#define SECRET_SSID "XXXXXXXXXXXXX"
-#define SECRET_PASS "XXXXXXXXXXXXX"
+#define SECRET_SSID "XXXXXXXXX"  // change it to your Wifi name
+#define SECRET_PASS "XXXXXXXXX"    // change it to your Wifi Password
